9422490f2e424a02e38fcba5162df7de600931ed
Augustto Santana <santana.acesar@gmail.com>
2025-07-31 01:59:34 -0300

    Add Connection layer of Delivery

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Item.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Item.java
@@ -1,27 +1,38 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import javax.swing.*;
 import java.util.UUID;
 
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@NoArgsConstructor(access = AccessLevel.PACKAGE)
-@Setter(AccessLevel.PRIVATE)
+@Entity
 @Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
     private String name;
+
     @Setter(AccessLevel.PACKAGE)
     private Integer quantity;
 
-    static Item brandNew(String name, Integer quantity) {
+    @ManyToOne(optional = false)
+    @Getter(AccessLevel.PRIVATE)
+    private Delivery delivery;
+
+    static Item brandNew(String name, Integer quantity, Delivery delivery) {
         Item item = new Item();
         item.setId(UUID.randomUUID());
         item.setName(name);
         item.setQuantity(quantity);
+        item.setDelivery(delivery);
         return item;
     }
 
